a1c6aaf89deb06abc5319c4203a0b8c3f7c8e9e3
Khushpreet Singh <127000509+khushramgaria@users.noreply.github.com>
2025-05-27 20:11:55 +0530

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -2,33 +2,58 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import  { PriceConvertor } from "./PriceConvertor.sol";
 
 contract FundMe {
 
-    uint256 public minimumUsd = 5e18;
+    using PriceConvertor for uint256;
+
+    uint256 public constant minimumUsd = 5e18;
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public immutable owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
         // Allow users to send $
         // Having a minimum $ sent
-        require(getConversionRate(msg.value) > minimumUsd, "Didn't send enough ETH"); // 1e18 === 1 ETH
+        require(msg.value.getConversionRate() > minimumUsd, "Didn't send enough ETH"); // 1e18 === 1 ETH
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function getPrice() public view returns (uint256) {
-        //Address  
-        //ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
+    function withdraw() public onlyOwner {
+        // update the values to 0
+        for(uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        //reset the array
+        funders = new address[](0);
+
+        //withdraw the funds
+        // 3 ways
+
+        // 1. transfer (automatic revert transaction if failed)
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // 2. send ( return bool if success or failed and not revert the transaction)
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // // to revert transaction instead of getting just bool
+        // require(sendSuccess, "Send failed");
+
+        // 3. call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
     }
 
-    function getConversionRate (uint256 ethAmount) public view returns (uint256) {
-        // 1 ETH ?
-        // if 1 ETH = 2000 USD then i get ethPrice as 2000_000000000000000000 WEI
-        uint256 ethPrice = getPrice();
-        // (2000_00000000000000000 * 1_000000000000000000) / 1e18
-        // $2000 = 1ETH
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+    modifier onlyOwner {
+        require(owner == msg.sender, "Only owner can withdraw !!");
+        _;
     }
-}
+} 
