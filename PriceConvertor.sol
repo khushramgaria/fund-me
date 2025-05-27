34f0308e41625677507b128188f74282fc8fdad9
Khushpreet Singh <127000509+khushramgaria@users.noreply.github.com>
2025-05-27 20:12:25 +0530

    Create PriceConvertor.sol

---
+++ PriceConvertor.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConvertor {
+    function getPrice() public view returns (uint256) {
+        //Address  
+        //ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate (uint256 ethAmount) public view returns (uint256) {
+        // 1 ETH ?
+        // if 1 ETH = 2000 USD then i get ethPrice as 2000_000000000000000000 WEI
+        uint256 ethPrice = getPrice();
+        // (2000_00000000000000000 * 1_000000000000000000) / 1e18
+        // $2000 = 1ETH
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
